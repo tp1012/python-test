150b84dda562124611b69b2be907fa024379540f
tp1012 <tp1012@gmail.com>
2025-09-17 03:53:29 +0000

    streamlit_20250917_2

--- udemy/pages/page2.py
+++ udemy/pages/page2.py
@@ -1,2 +1,19 @@
 import streamlit as st
-st.title("ページ２です")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+st.title = ("irisデータを用いた予測アプリ")
+
+iris = load_iris()
+x = pd.DataFrame(iris.data,columns=iris.feature_names)
+y = pd.Series(iris.target, name = "species")
+
+st.write(iris)
+st.write(x)
+st.write(ｙ)
+
